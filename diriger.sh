fa1eaaf769cd6630ee417eb583e25beabc833a09
Arsen Nuramatov <x@ctrl.sn>
2025-07-30 18:43:02 +0200

    fix: prompt from arg

--- diriger.sh
+++ diriger.sh
@@ -5,6 +5,7 @@ set -e
 _diriger_id="diriger-$(uuidgen)"
 
 feature="${1:?enter the feature name}"
+prompt="$2"
 root=$PWD
 projname=$(basename "$root")
 
@@ -19,5 +20,5 @@ for cmd in "${commands[@]}"; do
 	tmux neww -t "$_diriger_id" -n "$cmd" -c "$treepath" "$cmd"
 	sleep 2 # HACK: we need to sleep so that the interface loads and is ready to receive keystrokes
 	# TODO instead we should use `tmux capture-pane -t ... -p` to capture and continuously check whether the output has a matching string; every command will have it's own
-	tmux send -t "$_diriger_id:$cmd" "$2" Enter
+	tmux send -t "$_diriger_id:$cmd" "${prompt:?enter the prompt}" Enter
 done
